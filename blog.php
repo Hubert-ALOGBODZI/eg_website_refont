9f4c0422699c09784f7ba93c1b277db34ae032e1
Apelete <adzohonouapelete@gmail.com>
2022-06-26 19:10:19 +0000

    Menubar updated, mentor file created, newsletter's sent request page created&designed, newsletter form updated on all pages, & files reorganised

--- blog.php
+++ blog.php
@@ -1,28 +1,6 @@
-<?php
-   include('blog_connectDB.php');
-    if(isset($_POST['submit'])){
-        $name = $_POST['name'];
-        $mail = $_POST['mail'];
-        $checkbox1 = $_POST['checkbox1'];
-        $checkbox2 = $_POST['checkbox2'];
-
-        $sql = "insert into mabase (name, mail, checkbox1, checkbox2)
-                 values('$name' , '$mail', '$checkbox1', '$checkbox2')";
-
-                 
-        $result = mysqli_query($conn,$sql);
-        if($result){
-            echo"record saved";
-            // header('location:blog2.php');
-        }else{
-            die(mysqli_error($conn));
-        }
-    }
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,51 +10,62 @@
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
     <div class="container">
-<!--============================== Menu for all pages=========================================== -->
-    <div class="menu_bar">
-        <div class="logo">
-            <a href="index.html">
-                <img src="assets/logo.png" alt="eg_logo" class="logo_img">
-            </a>
+        <!-- Menu for all pages -->
+        <div class="menu_bar_bottom">
+            <li><a href="index.html">
+                    <img src="assets/house.svg" class="icon_ng"><span>Home</span></a></li>
+            <li><a href="business-energy.html">
+                    <img src="assets/school.svg" class="icon_ng"><span>Business & Energy School</span></a></li>
+            <li><a href="energy-space.html">
+                    <img src="assets/rocket.svg" class="icon_ng"><span>Energy Space</span></a></li>
+            <li>
+                <a href="blog.php" style="color: crimson;">
+                    <img src="assets/blog.svg" class="icon_ng"><span>Blog</span>
+                </a>
+            </li>
+            <li>
+                <a href="mentor.html" class="mentor">
+                    <img src="assets/mentor.svg" class="icon_ng"><span>Mentoring Program</span>
+                </a>
+            </li>
         </div>
-        <div class="menu_items">
-            <div class="menu">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="business-energy.html">Business & Energy School</a></li>
-                <li><a href="energy-space.html">Energy Space</a></li>
-                <li><a href="blog.html">Blog</a></li>
+        <div class="menu_bar">
+            <div class="logo">
+                <a href="index.html">
+                    <img src="./assets/logo.png" alt="eg_logo" class="logo_img">
+                </a>
             </div>
-            <li class="lang">
-                <span>
-                    <a href="#">
-                        <img src="assets/FRA.png" alt="french img" class="langfr">
-                    </a>
-                </span>
-            </li>
-            <li class="lang">
-                <span>
-                    <a href="#">
-                        <img src="assets/GBR.png" alt="eng img" class="langen">
-                    </a>
-                </span>
-            </li>
+            <div class="menu_items">
+                <div class="menu">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="business-energy.html">Business & Energy School</a></li>
+                    <li><a href="energy-space.html">Energy Space</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li>
+                        <a href="mentor.html" class="mentor">
+                            Mentoring Program
+                        </a>
+                    </li>
+                </div>
+            </div>
         </div>
-    </div>
-<!--======================================header for blog page===================================-->
-            <header>
-                <div class="left">
-                    <a href="#Posts">All Posts</a>
-                    <a href="#Scolaire">Formation Scolaire</a>
-                    <a href="#Formation">Formation</a>
-                </div>
-                <div class="right">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <button><a href="#">sign in / sign up</a></button>
-                </div>
-            </header>
-<!--======================================Articles for blog page===================================-->
+
+        <!-- End Menu for all pages -->
+        <header>
+            <div class="left">
+                <a href="#Posts">All Posts</a>
+                <a href="#Scolaire">Formation Scolaire</a>
+                <a href="#Formation">Formation</a>
+            </div>
+            <div class="right">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <button><a href="#">sign in / sign up</a></button>
+            </div>
+        </header>
+        <!--======================================Articles for blog page===================================-->
         <section class="articles" id="Posts">
             <div class="left">
                 <img src="assets/blogimg3.png ">
@@ -172,107 +161,109 @@
                 </div>
             </div>
         </section>
-         <!-- Start Footer section page -->
-    <div class="footer_section">
-        <div class="believers">
-            <div class="believer_title">
-                They believe in us !
+        <!-- Start Footer section page -->
+        <div class="footer_section">
+            <div class="believers">
+                <div class="believer_title">
+                    They believe in us !
+                </div>
+                <div class="believer_el">
+                    <div class="img1">
+                        <img src="assets/believer1.png">
+                    </div>
+                    <div class="img2">
+                        <img src="assets/believer2.png">
+                    </div>
+                    <div class="img3">
+                        <img src="assets/believer3.png">
+                    </div>
+                    <div class="img4">
+                        <img src="assets/believer4.png">
+                    </div>
+                    <div class="img5">
+                        <img src="assets/believer5.png">
+                    </div>
+                    <div class="img6">
+                        <img src="assets/believer6.png">
+                    </div>
+                    <div class="img7">
+                        <img src="assets/believer7.png">
+                    </div>
+                </div>
             </div>
-            <div class="believer_el">
-                <div class="img1">
-                    <img src="assets/believer1.png">
+            <div class="footer">
+                <div class="newsletter">
+                    <div class="newsletter_gtitle">Newsletter</div>
+                    <div class="newsletter_ltitle">Get our latest info, podcasts, advices...</div>
+                    <form action="news.php" method="post" class="newsletter_form">
+                        <input type="text" placeholder="First name" name="name">
+                        <input type="text" placeholder="Email" name="mail">
+                        <fieldset>
+                            <legend>Campus Language</legend>
+                            <label class="radio">
+                                <input type="radio" name="lang" value="fr">
+                                <span>Je préfère recevoir les informations en français</span>
+                            </label>
+                            <br>
+                            <label class="radio">
+                                <input type="radio" name="lang" value="en">
+                                <span>I wish to receive the content in English</span>
+                            </label>
+                        </fieldset>
+                        <input type="submit" class="subscribe_btn" value="Subscribe" name="submit">
+                    </form>
                 </div>
-                <div class="img2">
-                    <img src="assets/believer2.png">
+                <div class="social_links_section">
+                    <div class="logo_immg">
+                        <img src="assets/logo.png">
+                    </div>
+                    <div class="socials_link">
+                        <a href="#">
+                            <img src="assets/facebook.svg">
+                        </a>
+                        <a href="#">
+                            <img src="assets/twitter.svg">
+                        </a>
+                        <a href="#">
+                            <img src="assets/linkedin.svg">
+                        </a>
+                        <a href="#">
+                            <img src="assets/youtube.svg">
+                        </a>
+                    </div>
                 </div>
-                <div class="img3">
-                    <img src="assets/believer3.png">
+                <div class="all_links">
+                    <div class="all_link_title">
+                        Links
+                    </div>
+                    <ul class="site_links">
+                        <li><a href="index.html">Accueil</a></li>
+                        <li><a href="index.html">Programs</a></li>
+                        <li><a href="index.html">Energy Space</a></li>
+                        <li><a href="index.html">Blog</a></li>
+                    </ul>
                 </div>
-                <div class="img4">
-                    <img src="assets/believer4.png">
-                </div>
-                <div class="img5">
-                    <img src="assets/believer5.png">
-                </div>
-                <div class="img6">
-                    <img src="assets/believer6.png">
-                </div>
-                <div class="img7">
-                    <img src="assets/believer7.png">
+                <div class="copyright">
+                    © 2022 - Energy Generation - All rights reserved.
                 </div>
             </div>
         </div>
-        <div class="footer">
-            <div class="newsletter">
-                <div class="newsletter_gtitle">Newsletter</div>
-                <div class="newsletter_ltitle">Get our latest info, podcasts, advices...</div>
-                <form action="blog_db.php" method="post" class="newsletter_form" >
-                    <input type="text" placeholder="First name" name="name">
-                    <input type="text" placeholder="Email" name="mail">
-                    <fieldset>
-                        <label>
-                            <input type="checkbox" name="checkbox1">
-                            <span>Je préfère recevoir les informations en français</span>
-                        </label> 
-                        <br>
-                        <label>
-                            <input type="checkbox" name="checkbox2">
-                            <span>I wish to receive the content in English</span>
-                        </label>
-                    </fieldset>
-                    <input type="submit" class="subscribe_btn" value="Subscribe" name="submit">
-                </form>
-            </div>
-            <div class="social_links_section">
-                <div class="logo_immg">
-                    <img src="assets/logo.png">
-                </div>
-                <div class="socials_link">
-                    <a href="#">
-                        <img src="assets/facebook.svg">
-                    </a>
-                    <a href="#">
-                        <img src="assets/twitter.svg">
-                    </a>
-                    <a href="#">
-                        <img src="assets/linkedin.svg">
-                    </a>
-                    <a href="#">
-                        <img src="assets/youtube.svg">
-                    </a>
-                </div>
-            </div>
-            <div class="all_links">
-                <div class="all_link_title">
-                    Links
-                </div>
-                <ul class="site_links">
-                    <li><a href="index.html">Accueil</a></li>
-                    <li><a href="index.html">Programs</a></li>
-                    <li><a href="index.html">Energy Space</a></li>
-                    <li><a href="index.html">Blog</a></li>
-                </ul>
-            </div>
-            <div class="copyright">
-                © 2022 - Energy Generation - All rights reserved.
-            </div>
-        </div>
+        <!-- End Footer section page -->
     </div>
     <!-- End Footer section page -->
-    </div>
-        <!-- End Footer section page -->
 
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/ScrollTrigger.min.js"></script>
     <script src="script.js"></script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <script type="text/javascript">
         function googleTranslateElementInit() {
             new google.translate.TranslateElement({
-                pageLanguage: 'en'
+                pageLanguage: 'fr'
             }, 'google_translate_element');
         }
     </script>
 </body>
+
 </html>
--- blog_connectDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn =new mysqli("localhost","root", "", "Blog_DB");
-
-if($conn){
-    echo"connection successful";
-}else{
-    die(mysqli_error($conn));
-}
